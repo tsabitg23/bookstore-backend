d604b341a271350a7ff2be229fb4c025fafc0866
tsabit ghazwan <tsabit.ghazwan@thetreedots.com>
2024-04-03 10:45:44 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { AppDataSource } from "./data-source";
+import * as express from "express";
+import * as dotenv from "dotenv";
+import { Request, Response } from "express";
+import { userRouter } from "./routes/user.routes";
+import { movieRouter } from "./routes/movie.routes";
+import "reflect-metadata";
+import { errorHandler } from "./middleware/errorHandler";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const { PORT = 3000 } = process.env;
+app.use(errorHandler);
+app.use("/auth", userRouter);
+app.use("/api", movieRouter);
+
+app.get('/', (req,res) => {
+  res.status(200).json({message: "Hello world!"});
+})
+
+
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(PORT, () => {
+      console.log("Server is running on http://localhost:" + PORT);
+    });
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
